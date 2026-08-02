b85c8c30d4bf76009dac67d142816122caceb17d
HermanButrin <herman.butrin@gmail.com>
2026-08-02 13:15:06 +0200

    refactor: resolve E117

--- aiogram_bot_template/middlewares/user_activity.py
+++ aiogram_bot_template/middlewares/user_activity.py
@@ -24,9 +24,15 @@ class UserActivityMiddleware(BaseMiddleware):
         user = None
         if event.message is not None and event.message.from_user is not None:
             user = event.message.from_user
-        elif event.callback_query is not None and event.callback_query.from_user is not None:
+        elif (
+            event.callback_query is not None
+            and event.callback_query.from_user is not None
+        ):
             user = event.callback_query.from_user
-        elif event.edited_message is not None and event.edited_message.from_user is not None:
+        elif (
+            event.edited_message is not None
+            and event.edited_message.from_user is not None
+        ):
             user = event.edited_message.from_user
 
         if user is None:
@@ -37,7 +43,7 @@ class UserActivityMiddleware(BaseMiddleware):
             return await handler(event, data)
 
         with suppress(Exception):
-                await ensure_user_exists(db_pool, user.id)
-                await update_last_active(db_pool, user.id)
+            await ensure_user_exists(db_pool, user.id)
+            await update_last_active(db_pool, user.id)
 
         return await handler(event, data)
